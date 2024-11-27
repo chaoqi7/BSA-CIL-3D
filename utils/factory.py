2be9365d5c9e0bf757658eaec7d3b3ee67230e13
chaoqi7 <53529969+chaoqi7@users.noreply.github.com>
2024-11-27 09:36:23 +0800

    Delete utils directory

--- utils/factory.py
+++
@@ -1,8 +0,0 @@
-def get_model(model_name, args, model_config = None):
-    name = model_name.lower()
-    if name == "ease":
-        from models.ease import Learner
-    else:
-        assert 0
-    
-    return Learner(args, model_config = model_config)
